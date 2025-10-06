6d3a831d5069faca5bcd8b309b762e6770f1a285
vishal kumar <iamvishalnigam@gmail.com>
2025-10-06 11:03:49 +0530

    ok

--- server/src/utills/genToken.ts
+++ server/src/utills/genToken.ts
@@ -1,36 +1,34 @@
 import { Response } from "express";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 import { ApiResponse } from "../shared/types/apiResponse.type";
 import env from "../config/env";
 
-export const genToken =  (
-    userId: string | number | unknown,
-    role: string,
-    res: Response<ApiResponse<null>>,
-    message: string
+export const genToken = (
+  userId: string | number | unknown,
+  role: string,
+  res: Response<ApiResponse<null>>,
+  message: string
 ) => {
-    try {
-        const token = jwt.sign(
-            { userId, role },
-            env.JWT_SECRET,
-            { expiresIn: "1d" }
-        );
-        
-      res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",  // only over HTTPS in prod
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
-  maxAge: 24 * 60 * 60 * 1000, 
-});
-        
-        return res.status(200).json({
-            message,
-            success: true
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: "Failed to generate token",
-            success: false
-        });
-    }
-}
+  try {
+    const token = jwt.sign({ userId, role }, env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // only over HTTPS in prod
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      message,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to generate token",
+      success: false,
+    });
+  }
+};
